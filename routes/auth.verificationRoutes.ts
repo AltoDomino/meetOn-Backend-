b21fbd50a3d0cc2f04afe01cdb5a4fabcb05ec0d
Dominik <d.dduraj@gmail.com>
2025-07-18 09:24:38 +0200

    verify

--- routes/auth.verificationRoutes.ts
+++ routes/auth.verificationRoutes.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import { getRegister } from "../controllers/auth.registrationControllers";
 import { verifyEmail } from "../controllers/auth.verification.controller";
 
 const router = express.Router();
 
-router.post("/", getRegister);
+router.get("/verify-email", verifyEmail); 
 
 export default router;
