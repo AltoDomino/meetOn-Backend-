a55a8d0882dd4b7fab4d05f459c8594d33f99fab
Dominik <d.dduraj@gmail.com>
2025-07-22 08:38:32 +0200

    login

--- services/auth.loginService.ts
+++ services/auth.loginService.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
@@ -6,11 +7,13 @@ export const login = async (email: string, password: string) => {
   const user = await prisma.user.findUnique({ where: { email } });
 
   if (!user) {
-    throw new Error("Nie znaleziono użytkownika o podanym adresie e-mail.");
+    return null; // zamiast throw
   }
 
-  if (user.password !== password) {
-    throw new Error("Nieprawidłowe hasło.");
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) {
+    return null; // zamiast throw
   }
 
   return { id: user.id, email: user.email, userName: user.userName };
